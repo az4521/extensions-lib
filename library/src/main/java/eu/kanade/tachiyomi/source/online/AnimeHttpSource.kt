ac7e45e1fc1083c486dad7f959763e258ed8b640
az4521 <cheeze.sprinkels@gmail.com>
2021-01-03 05:42:30 -0500

    Update AnimeHttpSource.kt

--- library/src/main/java/eu/kanade/tachiyomi/source/online/AnimeHttpSource.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/online/AnimeHttpSource.kt
@@ -24,4 +24,38 @@ abstract class AnimeHttpSource : HttpSource() {
 
     abstract protected fun fetchSearchAnime(page: Int, query: String, filters: FilterList): Observable<MangasPage>
 
+    //renames
+    override fun fetchPopularManga(page:Int): Observable<MangasPage> = throw Exception("Stub!")
+
+    override fun fetchMangaDetails(manga: SManga): Observable<SManga> = throw Exception("Stub!")
+
+    override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> = throw Exception("Stub!")
+
+    override fun fetchPageList(chapter: SChapter): Observable<List<Page>> = throw Exception("Stub!")
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> = throw Exception("Stub!")
+
+    override fun fetchLatestUpdates(page: Int): Observable<MangasPage> = throw Exception("Stub!")
+
+    //unused
+    override fun imageUrlParse(response: Response) = throw Exception("Stub!")
+
+    override fun popularMangaRequest(page: Int): Request = throw Exception("Stub!")
+
+    override fun searchMangaParse(response: Response): MangasPage = throw Exception("Stub!")
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = throw Exception("Stub!")
+
+    override fun mangaDetailsParse(response: Response): SManga = throw Exception("Stub!")
+
+    override fun pageListParse(response: Response): List<Page> = throw Exception("Stub!")
+
+    override fun popularMangaParse(response: Response): MangasPage = throw Exception("Stub!")
+
+    override fun chapterListParse(response: Response): List<SChapter> = throw Exception("Stub!")
+
+    override fun latestUpdatesRequest(page: Int): Request = throw Exception("Stub!")
+
+    override fun latestUpdatesParse(response: Response): MangasPage = throw Exception("Stub!")
+
 }
